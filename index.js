a5aaa5e2d397dfccbecbbf3b3ddd9fb1fdc4d503
Anna <34191811+404unf@users.noreply.github.com>
2023-07-07 23:47:34 -0700

    Palindromes using .reverse()

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray = Array.from(word)
+  console.log(wordArray)
+  const backwardsArray = wordArray.reverse()
+  console.log(backwardsArray)
+
+  for (let i=0; i<wordArray.length;i++){
+    let a = wordArray[i]
+    let b = backwardsArray[(wordArray.length-1) - i]
+    if(a !== b){
+      return false
+    }
+  }
+
+  return true
 }
 
 /* 
   Add your pseudocode here
 */
+// Split a string into an array
+// Create an inverse array
+// Compare each item in the array at the same index
+// If any item is not equal, the string is not a palindrome
+// Otherwise, return true
 
 /*
   Add written explanation of your solution here
